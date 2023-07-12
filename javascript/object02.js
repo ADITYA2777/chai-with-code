d5a3519b7fbfabc1fd6dce496b355023d8edf6a4
ADITYA2777 <ajadityajain01@gmail.com>
2023-07-12 07:42:18 -0700

    json

---
+++ javascript/object02.js
@@ -0,0 +1,19 @@
+
+
+const course ={
+   coursename:"js hindi objecr",
+   price:"9000",
+   courseInstructor:"aditya"
+}
+// course.courseInstructor
+const {courseInstructor:int}= course
+// console.log(courseInstructor);
+// console.log(int);
+
+// react concept for example
+
+// const navBar = ({company}) =>{
+
+// }
+// navBar(company = "aditya")
+// console.log(company);
